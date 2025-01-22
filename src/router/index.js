1112dde37e698bf6e1df8272bbb149708f02b8b2
LeglamO <64050502@kmitl.ac.th>
2025-01-22 21:44:34 +0700

    Set Zero

--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,16 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: "/cookies",
-      name: "cookies",
-      component: () => import("../views/Policies/Cookie.vue"),
-    },
-    {
-      path: "/exchange",
-      name: "exchange",
-      component: () => import("../views/Policies/ExchangeReturn.vue"),
-    },
+    // {
+    //   path: "/cookies",
+    //   name: "cookies",
+    //   component: () => import("../views/Policies/Cookie.vue"),
+    // },
+    // {
+    //   path: "/exchange",
+    //   name: "exchange",
+    //   component: () => import("../views/Policies/ExchangeReturn.vue"),
+    // },
   ],
 });
 
