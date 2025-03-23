05196b56bc611645b6a87e4d01f77b5d10650d00
64050679 <64050679@kmitl.ac.th>
2025-03-23 14:45:11 +0700

    [back design]

--- src/components/Card-Component.vue
+++ src/components/Card-Component.vue
@@ -50,7 +50,7 @@
                 @change="handleCheckbox"
                 :disabled="isDisable"
               />
-              <span>เปรียบเทียบ A</span>
+              <span>เปรียบเทียบ</span>
             </label>
 
             <a
